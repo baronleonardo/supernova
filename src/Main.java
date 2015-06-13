db955f5996ddb733c3b01de643b3cc6fdd3e10e8
Baron Leonardo <mohamedayman.fcis@zoho.com>
2015-06-13 13:46:23 +0200

    remove the tested link and the tested path

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main
 {
     public static void main(String[] args) throws InterruptedException, IOException
     {
-        String download_link = "https://github.com/aptana/studio3/releases/download/v3.6.1/Aptana_Studio_3_Setup_Linux_x86_64_3.6.1.zip";
-        String file_path = "/tmp/GWorkspace.png";
+        String download_link = "LINK";
+        String file_path = "PATH/file";
         int number_of_segments = 3;
 
         DownLoadManagerPreparing downloader =
